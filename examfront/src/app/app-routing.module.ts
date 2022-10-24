b613620d47e0ac889c2e19a52baddfdb36dff5ab
Jitendrakrverma1 <jitendrakrverma06@gmail.com>
2022-10-24 10:06:32 -0400

    first commit

---
+++ examfront/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SingupComponent } from './pages/singup/singup.component';
+
+const routes: Routes = [
+  
+  {
+     path: '',
+     component: HomeComponent,
+     pathMatch:'full',
+  },
+
+  {
+    path: 'signup',
+    component: SingupComponent,
+    pathMatch:'full',
+  },
+  {
+    path: 'login',
+    component:LoginComponent,
+    pathMatch: 'full',
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
